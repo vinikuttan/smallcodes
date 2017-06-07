42ce0107ae0c40c382abb0917186dee6dd73fbb5
vinikuttan <vinesh.emag@gmail.com>
2017-06-07 18:07:58 +0530

    Adding freeze functionality

--- helper.py
+++ helper.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+""" 
+Generic Utility functionalities
+"""
 
 
 def deep_getattr(obj, attr, default=None):
@@ -14,7 +17,30 @@ def deep_getattr(obj, attr, default=None):
         except AttributeError:
             return default
     return obj
-    
 
-        
-    
+
+class FrozenDict(dict):
+    def __hash__(self):
+        return hash(tuple(sorted(self.items())))
+
+    def _immutable(self, *args, **kws):
+        raise TypeError('object is immutable')
+
+    __setitem__ = _immutable
+    __delitem__ = _immutable
+    clear = _immutable
+    update = _immutable
+    setdefault = _immutable
+    pop = _immutable
+    popitem = _immutable
+
+
+def freeze(obj):
+    if isinstance(obj, dict):
+        return FrozenDict((k, freeze(v)) for k, v in obj.items())
+    elif isinstance(obj, list):
+        return tuple(freeze(el) for el in obj)
+    elif isinstance(obj, set):
+        return frozenset(obj)
+    else:
+        return obj   
