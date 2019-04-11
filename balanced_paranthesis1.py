54a05c0d91097424b0ab939419d5a76c3dfe18b8
vinikuttan <vinesh.emag@gmail.com>
2019-04-11 09:05:30 +0530

    Simple paranthesis balancer

---
+++ balanced_paranthesis1.py
@@ -0,0 +1,45 @@
+#/usr/bin/python
+
+class Stack(object):
+    """ Stack """
+    def __init__(self):
+        self.items = list()
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+
+def balanced_paranthesis(expression):
+    """ Simple algorithm to find balance paranthesis """
+
+    stack = Stack()
+    for i in expression:
+        if i  == "(":
+            stack.push(i)
+
+        elif i == ")":
+            if stack.is_empty():
+                return False
+            stack.pop()
+
+    return True if stack.is_empty() else False
+
+
+if __name__=="__main__":
+    expression = "((A+B)-(C+D))"
+    if balanced_paranthesis(expression):
+        print "Paranthesis balanced in expression"
+    else:
+        print "Imbalanced Paranthesis"
